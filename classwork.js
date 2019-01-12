545a7f3d60df4206475220b7abc2f4eaa8ce47f1
Maniccia <mariamaniccia@gmail.com>
2019-01-12 12:58:30 -0500

    playGame working

--- classwork.js
+++ classwork.js
@@ -14,10 +14,19 @@ function Player(name, position, offense, defense) {
 }
 
 var count = 0;
+var gameCount = 0;
 
+var score = 0;
 var starterArr = [];
 var subArr = [];
 
+function getRandomNumberOne() {
+    return getRandomNumber(1,20);
+};
+
+function getRandomNumberTwo() {
+    return getRandomNumber(1,20);
+};
 function createPlayer() {
     if (count < 3) {
         console.log("Create a new player:");
@@ -30,10 +39,12 @@ function createPlayer() {
                 message: "What is your current position?"
             }, {
                 name: "offense",
-                message: "What is your attack power? From 1-10:"
+                message: "What is your attack power? From 1-10:",
+                default: "number"
             }, {
                 name: "defense",
-                message: "What is your armor strength? From 1-10:"
+                message: "What is your armor strength? From 1-10:",
+                default: "number"
             }
         ]).then(function (answers) {
 
@@ -68,8 +79,57 @@ function createPlayer() {
             subArr[i].printStats();
             console.log("----------------");
         }
-        
+        playGame();
     }
 }
+
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+}
+
+function playGame() {
+    if (gameCount < 5) {
+        var randNumberOne = getRandomNumberOne();
+        var randNumberTwo = getRandomNumberTwo();
+
+        var offSum = 0;
+        var defSum = 0;
+
+        // console.log(randNumberOne);
+        // console.log(randNumberTwo);
+
+        for (var j = 0; j < starterArr.length; j++) {
+            // console.log(starterArr[j].offense);
+            offSum += parseInt(starterArr[j].offense);
+        }
+        // console.log(offSum);
+        for (var d = 0; d < starterArr.length; d++) {
+            defSum += parseInt(starterArr[d].defense);
+        }
+        // console.log(defSum);
+        if (randNumberOne < offSum) {
+            score++;
+            console.log("You gained a point!  Score: " + score);
+        } else {
+            console.log("Your offense wasn't enough!");
+        }
+        if (randNumberTwo > defSum){
+            score--;
+            console.log("You lost a point!  Score: " + score);
+        } else {
+            console.log("Your defense wasn't enough!");
+        }
+        console.log("Your current score: " + score);
+        gameCount++;
+        playGame();
+        
+    }
+
+}
+
 
 createPlayer();
